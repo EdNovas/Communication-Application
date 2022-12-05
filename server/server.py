50964419bf0a03247648b2ee1193fc13e911e2d0
Erik McKelvey <43917961+Erik-McKelvey@users.noreply.github.com>
2022-12-04 22:58:33 -0800

    Fixed sockets

--- server/server.py
+++ server/server.py
@@ -1,9 +1,7 @@
-
 import os
 import csv
 import threading
 import socket
-
 
 host = '172.18.0.4'
 port = 59000
@@ -13,15 +11,8 @@ print('Socket binded.')
 server.listen()
 print('Server is running and listening ...')
 clients = []
-aliases = []
-
-
-def broadcast(message):
-    for client in clients:
-        if message == 'leave':
-            break
-        else:
-            client.sendall(message)
+usernames = []
+isLoggedIn = []
 
 
 
@@ -29,49 +20,43 @@ def handle_client(client):
     while True:
         try:
             message = client.recv(1024)
-            broadcast(message)
-            if message == 'leave':
-                index = clients.index(client)
-                clients.remove(client)
-                client.close()
-                alias = aliases[index]
-                broadcast(f'{alias} has left the chat room!'.encode('utf-8'))
-                aliases.remove(alias)
+            parse_message(message)
+            if message == "q":
+                remove_client(client)
                 break
         except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            alias = aliases[index]
-            broadcast(f'{alias} has left the chat room!'.encode('utf-8'))
-            aliases.remove(alias)
+            remove_client(client)
             break
+
+def remove_client():
+    index = clients.index(client)
+    clients.remove(client)
+    client.close()
+    username = usernames[index]
+    usernames.remove(alias)
+
 
 
 def main():
     while True:
         client, address = server.accept()
-        conn_msg = 'connection is established with' + str(address)
+        conn_msg = 'Connection is established with' + str(address)
         print(conn_msg)
 
-        client.sendall('alias?'.encode('utf-8'))
-        alias = client.recv(1024)
-        aliases.append(alias)
+        usernames.append("")
         clients.append(client)
-
-        alias_msg = 'The alias of this client is' + str(alias)
-        print(alias_msg)
+        isLoggedIn.append(False)
         
-        broadcast_msg = str(alias) + 'has connected to the server'
-        broadcast(broadcast_msg.encode('utf-8'))
-        
-        client.sendall('you are now connected!'.encode('utf-8'))
         thread = threading.Thread(target=handle_client, args=(client,))
         thread.start()
 
 
 if __name__ == "__main__":
     main()
+
+
+def parse_message():
+    # TODO copy from client.py and make required modifications
 
 
 # print("Hello from server")
