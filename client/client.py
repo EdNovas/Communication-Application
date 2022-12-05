ecd77669e9ab5aa84486d36017720f94cd9d95dd
Erik McKelvey <43917961+Erik-McKelvey@users.noreply.github.com>
2022-12-05 00:12:53 -0800

    Completed server message parsing, and some client message parsing

--- client/client.py
+++ client/client.py
@@ -168,7 +168,7 @@ def get_register_message(username, rsa_public_key):
     if len(username) > 16: raise Exception("Username must be max 16 characters")
 
     padded_username = pad_string(username)
-    key_string = str(rsa_get_public_bytes(rsa_public_key))
+    key_string = rsa_get_public_bytes(rsa_public_key).decode('utf-8')
     return "r" + padded_username + key_string
 
 # Get string in the following format: "r"[16 bytes username]
@@ -183,7 +183,7 @@ def get_login_message2(username, signature):
     if len(username) > 16: raise Exception("Username must be max 16 characters")
     
     padded_username = pad_string(username)
-    signature_str = str(signature)
+    signature_str = signature.decode('utf-8')
     return "s" + padded_username + signature_str
 
 # Get string in the following format: "m"[16 bytes username][16 bytes rsa signature][DH public key] 
@@ -192,16 +192,16 @@ def get_message1(username, rsa_signature, dh_public_key):
     if len(rsa_signature) != 16: raise Exception("RSA signature must be of length 16")
     
     padded_username = pad_string(username)
-    rsa_signature_str = str(rsa_signature)
-    dh_public_key_str = str(dh_get_public_bytes(dh_public_key))
+    rsa_signature_str = rsa_signature.decode('utf-8')
+    dh_public_key_str = dh_get_public_bytes(dh_public_key).decode('utf-8')
     return "m" + padded_username + rsa_signature_str + dh_public_key_str
 
 # Get string in the following format: "b"[16 bytes rsa signature][16 bytes iv][DH public key] 
 def get_message1_response(rsa_signature, dh_public_key):
     if len(rsa_signature) != 16: raise Exception("RSA signature must be of length 16")
     
-    rsa_signature_str = str(rsa_signature)
-    dh_public_key_str = str(dh_get_public_bytes(dh_public_key))
+    rsa_signature_str = rsa_signature.decode('utf-8')
+    dh_public_key_str = dh_get_public_bytes(dh_public_key).decode('utf-8')
     return "b" + rsa_signature_str + dh_public_key_str
 
 # Get string in the following format: "n"[16 bytes hmac signature][16 bytes iv][Encrypted message]
@@ -209,32 +209,40 @@ def get_message2(hmac, iv, message):
     if len(hmac) != 16: raise Exception("HMAC must be of length 16")
     if len(iv) != 16: raise Exception("IV must be of length 16")
     
-    hmac_str = str(hmac)
-    iv_str = str(iv)
-    message_str = str(message)
+    hmac_str = hmac.decode('utf-8')
+    iv_str = iv.decode('utf-8')
+    message_str = message.decode('utf-8')
     return "n" + padded_username + hmac_str + iv_str + message_str
 
 def parse_message(message):
-    if (message[0] == "r"):
-        padded_username_r = message[1:17]
-        key_string = message[17:]
-    elif (message[0] == "l"):
-        padded_username_l == message[1:]
-    elif (message[0] == "s"):
-        padded_username_s = message[1:17]
-        signature_str = message[17:]
+    if (message[0] == "l"):
+        # Login part 1 response
+        nonce = message[1:]
+        # TODO get RSA private key and send signature of nonce
+    
+    elif (message[0] == "e"):
+        # Error message from server
+        print_msg = message[1:]
+        print(print_msg)
+
     elif (message[0] == "m"):
+        # Message part 1
         padded_username_m = message[1:17]
         rsa_signature_str_m = message[17:33]
         dh_public_key_str = message[33:]
+
     elif (message[0] == "b"):
+        # Message part 1 response
         rsa_signature_str_b = message[1:17]
         dh_public_key_str_b = message[17:]
+
     elif (message[0] == "n"):
+        # Message part 2
         padded_username_n = message[1:17]
         hmac_str = message[17:33]
         iv_str = message[33:49]
         message_str = message[49:]
+        
     else:
         print("not a valid message")
     # TODO
